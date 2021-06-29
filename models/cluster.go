57278dc3a08bcedc442a91b0123fe0801528d1df
Marian Schwarz <marian.schwarz@sap.com>
2021-06-29 11:53:41 +0200

    expose cluster site

--- models/cluster.go
+++ models/cluster.go
@@ -29,6 +29,7 @@ type Cluster struct {
 		LastUpdated         string      `json:"last_updated"`
 		DeviceCount         int         `json:"device_count"`
 		Tenant              Tenant      `json:"tenant"`
+		Site				NestedSite	`json:"site"`
 		Type                NestedClusterType `json:"type"`
 		VirtualMachineCount int         `json:"virtualmachine_count"`
 		VlanCount           int         `json:"vlan_count"`
