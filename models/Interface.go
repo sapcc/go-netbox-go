2752b30865fbbf8e02133483673ff48f5d31d85e
Dmitri Fedotov <dmitri.fedotov@sap.com>
2023-07-10 17:08:27 +0300

    WritableInterface fix

--- models/Interface.go
+++ models/Interface.go
@@ -55,22 +55,22 @@ type ListInterfacesResponse struct {
 }
 
 type WritableInterface struct {
-	Cable         NestedCable     `json:"cable,omitempty"`
-	CustomFields  interface{}     `json:"custom_fields,omitempty"`
-	Description   string          `json:"description,omitempty"`
-	Device        int             `json:"device"`
-	Enabled       bool            `json:"enabled,omitempty"`
-	Label         string          `json:"label,omitempty"`
-	Lag           NestedInterface `json:"lag,omitempty"`
-	MacAddress    string          `json:"mac_address,omitempty"`
-	MarkConnected bool            `json:"mark_connected,omitempty"`
-	MgmtOnly      bool            `json:"mgmt_only,omitempty"`
-	Mode          InterfaceMode   `json:"mode,omitempty"`
-	Mtu           int             `json:"mtu,omitempty"`
-	Name          string          `json:"name"`
-	Parent        int             `json:"parent,omitempty"`
-	TaggedVlans   []NestedVLAN    `json:"tagged_vlans"`
-	Tags          []NestedTag     `json:"tags,omitempty"`
-	Type          InterfaceType   `json:"type"`
-	UntaggedVlan  NestedVLAN      `json:"untagged_vlan,omitempty"`
+	Cable         NestedCable `json:"cable,omitempty"`
+	CustomFields  interface{} `json:"custom_fields,omitempty"`
+	Description   string      `json:"description,omitempty"`
+	Device        int         `json:"device"`
+	Enabled       bool        `json:"enabled,omitempty"`
+	Label         string      `json:"label,omitempty"`
+	Lag           int         `json:"lag,omitempty"`
+	MacAddress    string      `json:"mac_address,omitempty"`
+	MarkConnected bool        `json:"mark_connected,omitempty"`
+	MgmtOnly      bool        `json:"mgmt_only,omitempty"`
+	Mode          string      `json:"mode,omitempty"`
+	Mtu           int         `json:"mtu,omitempty"`
+	Name          string      `json:"name"`
+	Parent        int         `json:"parent,omitempty"`
+	TaggedVlans   []int       `json:"tagged_vlans"`
+	Tags          []NestedTag `json:"tags,omitempty"`
+	Type          string      `json:"type"`
+	UntaggedVlan  int         `json:"untagged_vlan,omitempty"`
 }
