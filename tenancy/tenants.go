e01e6360bc5c44ad45055c2a2f9d8bc510199541
Alexandru Mihai <alexandru.mihai@sap.com>
2023-11-12 19:02:02 +0200

    added GetTenant

--- tenancy/tenants.go
+++ tenancy/tenants.go
@@ -3,13 +3,40 @@ package tenancy
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/sapcc/go-netbox-go/models"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+
+	"github.com/sapcc/go-netbox-go/models"
 )
 
-func (c *Client) ListTenants (opts models.ListTenantsRequest) (*models.ListTenantsResponse,error) {
-	request, err := http.NewRequest("GET", c.BaseUrl.String() + basePath + "tenants/", nil)
+func (c *Client) GetTenant(id int) (*models.Tenant, error) {
+	request, err := http.NewRequest("GET", c.BaseUrl.String()+basePath+"tenants/"+strconv.Itoa(id), nil)
+	if err != nil {
+		return nil, err
+	}
+	c.SetAuthToken(&request.Header)
+	response, err := c.HttpClient.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	if response.StatusCode != 200 {
+		return nil, fmt.Errorf("unexpected return code of %d", response.StatusCode)
+	}
+	var resObj = models.Tenant{}
+	bytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(bytes, &resObj)
+	if err != nil {
+		return nil, err
+	}
+	return &resObj, nil
+}
+
+func (c *Client) ListTenants(opts models.ListTenantsRequest) (*models.ListTenantsResponse, error) {
+	request, err := http.NewRequest("GET", c.BaseUrl.String()+basePath+"tenants/", nil)
 	if err != nil {
 		return nil, err
 	}
@@ -17,7 +44,7 @@ func (c *Client) ListTenants (opts models.ListTenantsRequest) (*models.ListTenan
 	setListTenantsParams(request, opts)
 	response, err := c.HttpClient.Do(request)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 	if response.StatusCode != 200 {
 		return nil, fmt.Errorf("unexpected return code of %d", response.StatusCode)
--- tenancy/tenants_test.go
+++ tenancy/tenants_test.go
@@ -1,13 +1,30 @@
 package tenancy
 
 import (
+	"os"
+	"testing"
+
 	"github.com/sapcc/go-netbox-go/models"
 	"github.com/seborama/govcr"
 	"github.com/stretchr/testify/assert"
-	"os"
-	"testing"
 )
 
+func TestClient_GetTenant(t *testing.T) {
+	client, err := New(os.Getenv("NETBOX_URL"), os.Getenv("NETBOX_TOKEN"), true)
+	if err != nil {
+		t.Fatal(err)
+	}
+	vcrConf := &govcr.VCRConfig{}
+	vcrConf.Client = client.HttpClient
+	vcr := govcr.NewVCR("ListTenants", vcrConf)
+	client.HttpClient = vcr.Client
+	res, err := client.GetTenant(17)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(res)
+	//assert.NotEqual(t, 0, res.Count)
+}
 func TestClient_ListTenants(t *testing.T) {
 	client, err := New(os.Getenv("NETBOX_URL"), os.Getenv("NETBOX_TOKEN"), true)
 	if err != nil {
